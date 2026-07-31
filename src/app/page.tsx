d5b3ad261d2c203a5c5eae30a542dd3d45c536d0
Michael Starke <accessoverexcess@gmail.com>
2026-07-31 13:24:05 -0400

    Adjust featured collection layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 const products = [
   {
     name: "Private Jet Hoodie",
-    image: "/hoodie private jet.PNG",
+    image: "/hoodieprivatejet.PNG",
   },
   {
     name: "Cream Hoodie",
@@ -94,7 +94,7 @@ export default function Home() {
                   <img
                     src={product.image}
                     alt={product.name}
-                    className="h-full w-full object-cover transition duration-700 group-hover:scale-[1.025]"
+                    className="h-full w-full object-contain p-4 transition duration-700 group-hover:scale-[1.025]"
                   />
                 </div>
 
